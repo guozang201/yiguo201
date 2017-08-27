eda45115fdd686eda0a6570ca33e7252cddb1671
huangming6220@gmail.com <huangming@biomaps.rutgers.edu>
2017-08-27 02:24:05 -0400

    add pdf

--- scripts/libre.sh
+++ scripts/libre.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# run under qq/
+# sh ../scripts/libre.sh html
+# sh ../scripts/libre.sh txt 
+
 fmt=$1
 
 if [ ! -d $fmt ]; then
@@ -10,19 +14,27 @@ cd $fmt
 
 for dir in `ls -d ../articles/*/`; do
     echo "$dir"
-    name=`basename "$dir"`
+    dirname=`basename "$dir"`
     
-    if [ ! -d "$name" ]; then
-       mkdir "$name"
+    if [ ! -d "$dirname" ]; then
+       mkdir "$dirname"
     fi
    
-    cd $name
+    cd "$dirname"
     for file in "../$dir"/*doc*; do
-        echo $file
-        libreoffice --headless --convert-to $fmt "$file"
+        filename=`basename $file`
+        if [ ! -d "$filename" ]; then
+           echo "$filename"
+           if [[ $fmt =~ 'txt' ]]; then
+              libreoffice --headless --convert-to $fmt:Text "$file"
+           else
+              libreoffice --headless --convert-to $fmt "$file"
+           fi
+        fi
     done
     cd ..
   
 done
 
 cd ..
+
